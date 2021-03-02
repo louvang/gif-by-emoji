b7e82251c4b2389d3dc60e703b1fe649814d1dd3
lumine <lou@louvang.com>
2021-03-02 14:52:36 -0600

    Complete initial working version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gif-by-emoji
-A webapp that lets you find a gif by emoji
+# [Gif By Emoji](https://louvang.github.io/gif-by-emoji)
+
+A webapp that generates a random gif based on the emoji selected by the user.
+
+<p align="center"><a href="https://louvang.github.io/gif-by-emoji/" target="_blank"><img src="https://louvang.github.io/gif-by-emoji/preview.png" alt="Gif By Emoji Preview" width="750px" /></a></p>
+
+## About Code
+
+Gif By Emoji uses the [Giphy API](https://developers.giphy.com/) to generate a .gif file based on a keyword tied to an emoji. The code is fairly short and simple with a function that grabs the URL of the gif from the API and displays it.
+
+Event listeners are added to each emoji. When clicked, the 'emotion' of the emoji appears in text beneath the .gif image and an active marker is applied to the current emoji selected.
---
+++ js/script.js
@@ -0,0 +1,32 @@
+const img = document.querySelector('#random-gif');
+const meaningContainer = document.querySelector('.meaning-container');
+const emojiCells = document.querySelectorAll('.emoji-cell');
+
+const getGif = (keyword) =>
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=qgD3y8UwPD8uqPX82ko6ybTwtlgO8Efm&s=${keyword}`, {
+    mode: 'cors',
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+
+emojiCells.forEach((emoji) => {
+  emoji.addEventListener('click', () => {
+    const emotion = emoji.id;
+    const otherActiveMarker = document.querySelector('.active-marker');
+    const thisActiveMarker = document.querySelector(`#${emotion} .inactive-marker`);
+
+    if (otherActiveMarker) {
+      otherActiveMarker.classList.add('inactive-marker');
+      otherActiveMarker.classList.remove('active-marker');
+    }
+
+    thisActiveMarker.classList.add('active-marker');
+    thisActiveMarker.classList.remove('inactive-marker');
+    meaningContainer.textContent = `"${emotion}"`;
+    getGif(emotion);
+  });
+});
